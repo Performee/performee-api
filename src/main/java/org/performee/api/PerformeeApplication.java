83f93ebca643b7460c2ba97c2d6cd5b66069367a
ekrylovich <work.egor@gmail.com>
2022-06-04 21:08:35 +0300

    Initial commit

---
+++ src/main/java/org/performee/api/PerformeeApplication.java
@@ -0,0 +1,11 @@
+package org.performee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PerformeeApplication.class, args);
+    }
+}
